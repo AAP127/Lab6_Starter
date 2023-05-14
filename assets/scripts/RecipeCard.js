3af95627abf8dc8a4c987f003e2ea388e934ccea
AAP127 <apestonj@gmail.com>
2023-05-13 20:27:20 -0700

    Star url

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -148,7 +148,7 @@ class RecipeCard extends HTMLElement {
       
       <div class="rating">
         <span>${data.rating}</span>
-        <img src="assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+        <img src="Lab6_Starter/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
         <span>(${data.numRatings})</span>
       </div>
       
